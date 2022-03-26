7c6d2d74047c9eee8100e737e83abbb35fd21790
Kevin Maxi <kmaxi@pichincha.com>
2022-03-25 21:16:16 -0500

    Generate necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './character/character.component';
+import { CharacterFormComponent } from './character-form/character-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    CharacterFormComponent
   ],
   imports: [
     BrowserModule

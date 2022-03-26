d329a0bc32ced57fc7f507eb0831da466f9c8c28
Kevin Maxi <kmaxi@pichincha.com>
2022-03-26 00:53:10 -0500

    Move components to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CharacterComponent } from './character/character.component';
-import { CharacterFormComponent } from './character-form/character-form.component';
-import { CharacterListComponent } from './character-list/character-list.component';
+import { CharacterComponent } from './components/character/character.component';
+import { CharacterFormComponent } from './components/character-form/character-form.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
 
 @NgModule({
   declarations: [

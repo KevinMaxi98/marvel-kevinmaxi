31b18510b258d76bdcfcc3e4880576ac6ec5d5a3
Kevin Maxi <kmaxi@pichincha.com>
2022-03-26 10:22:24 -0500

    Change idAuthor

--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -9,25 +9,26 @@ import {HttpClient} from "@angular/common/http";
 })
 export class CharactersService {
   public characters: CharacterInterface[] = []
+  idAuthor = "kevin.maxi@devsu.com"
   public charactersObserver: Subject<CharacterInterface[]>
   constructor(private http: HttpClient) {
     this.charactersObserver = new Subject();
   }
 
   async createCharacter(character: CharacterInterface){
-    this.http.post(`https://bp-marvel-api.herokuapp.com/marvel-characters?idAuthor=0106604416`, character, {}).subscribe(responseData => {
+    this.http.post(`https://bp-marvel-api.herokuapp.com/marvel-characters?idAuthor=${this.idAuthor}`, character, {}).subscribe(responseData => {
       console.log(responseData)
     })
   }
 
   async editCharacter(character: CharacterInterface){
-    this.http.put(`https://bp-marvel-api.herokuapp.com/marvel-characters/${character._id}?idAuthor=0106604416`, character, {}).subscribe(responseData => {
+    this.http.put(`https://bp-marvel-api.herokuapp.com/marvel-characters/${character._id}?idAuthor=${this.idAuthor}`, character, {}).subscribe(responseData => {
       console.log(responseData)
     })
   }
 
   async getCharacters(){
-    return this.http.get<{ [key: number]: CharacterInterface}>(`https://bp-marvel-api.herokuapp.com/marvel-characters?idAuthor=0106604416`).pipe(map(characters => {
+    return this.http.get<{ [key: number]: CharacterInterface}>(`https://bp-marvel-api.herokuapp.com/marvel-characters?idAuthor=${this.idAuthor}`).pipe(map(characters => {
       const charactersArray: CharacterInterface[] = [];
       for (const character in characters){
         charactersArray.push({...characters[character]})
@@ -40,7 +41,7 @@ export class CharactersService {
   }
 
   async deleteCharacter(characterId: string){
-    this.http.delete(`https://bp-marvel-api.herokuapp.com/marvel-characters/${characterId}?idAuthor=0106604416`).subscribe(responseData => {
+    this.http.delete(`https://bp-marvel-api.herokuapp.com/marvel-characters/${characterId}?idAuthor=${this.idAuthor}`).subscribe(responseData => {
       console.log(responseData)
     })
   }
